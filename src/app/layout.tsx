fe5e332903cbb61e5a80b8306ba28d1f7c1e59eb
Agustin-desktop <agustin2051@gmail.com>
2024-06-16 16:15:27 -0300

    Various

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,93 @@
 import type { Metadata } from "next";
-import {
-  IBM_Plex_Sans,
-  Inter,
-  Playfair_Display,
-  Quicksand,
-} from "next/font/google";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { AuthProvider } from "./api/AuthContext";
-import { ToastContainer } from "react-toastify";
+
 import ToastProvider from "./ToastProvider";
 
-const inter = Inter({ subsets: ["latin"] });
-const pair = Playfair_Display({ subsets: ["latin"] });
-const ibm = IBM_Plex_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 const quik = Quicksand({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mailprex | Send Emails from your Website with Ease",
   description:
-    "Mailprex is an email sending platform designed to allow website owners to quickly and easily send emails through forms on the front end.",
+    "Mailprex is the ideal platform for sending emails from your website. Our user-friendly API integrates into your front end, enabling quick and secure form submissions. We offer advanced customization and robust security to protect your data. Simplify your email sending process with Mailprex, your reliable and efficient solution.",
+  keywords: [
+    "email sending platform",
+    "email sending from websites",
+    "email sending API",
+    "front end email integration",
+    "web form email sending",
+    "email service for websites",
+    "email marketing API",
+    "email customization",
+    "email sending security",
+    "web form data privacy",
+    "secure email sending",
+    "email solutions for developers",
+    "email platform for web developers",
+    "email automation",
+    "email management for websites",
+    "email hooks for React",
+    "form sending hook in React",
+    "email service for web developers",
+    "custom email sending",
+    "email sending tool for websites",
+    "plataforma de envío de correos electrónicos",
+    "envío de correos electrónicos desde sitios web",
+    "API de envío de correos electrónicos",
+    "integración de correo electrónico en front end",
+    "envío de formularios web",
+    "servicio de correo electrónico para sitios web",
+    "API de email marketing",
+    "personalización de correos electrónicos",
+    "seguridad en envío de correos electrónicos",
+    "privacidad en formularios web",
+    "envío seguro de correos electrónicos",
+    "soluciones de email para desarrolladores",
+    "plataforma de correo para desarrolladores web",
+    "automatización de correos electrónicos",
+    "gestión de correos electrónicos para sitios web",
+    "email hooks para React",
+    "hook de envío de formularios en React",
+    "servicio de correo para desarrolladores web",
+    "envío de correos electrónicos personalizado",
+    "herramienta de envío de correos para sitios web",
+  ],
+  authors: {
+    name: "Agustín Avellaneda",
+  },
+  publisher: "Agustín Avellaneda",
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Mailprex  | Send Emails from your Website with Ease ",
+    description:
+      "Mailprex is the ideal platform for sending emails from your website. Our user-friendly API integrates into your front end, enabling quick and secure form submissions. We offer advanced customization and robust security to protect your data. Simplify your email sending process with Mailprex, your reliable and efficient solution.",
+    creator: "Avellaneda Agustín",
+    images:
+      "https://res.cloudinary.com/draig/image/upload/v1718564493/mailprex/ecqm348liaxh626yzscs.png",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+  },
+
+  openGraph: {
+    type: "website",
+    url: "https://mailprex.top",
+    title: "Mailprex  | Send Emails from your Website with Ease ",
+    description:
+      "Mailprex is the ideal platform for sending emails from your website. Our user-friendly API integrates into your front end, enabling quick and secure form submissions. We offer advanced customization and robust security to protect your data. Simplify your email sending process with Mailprex, your reliable and efficient solution.",
+    siteName: "Mailprex  | Send Emails from your Website with Ease",
+    images: [
+      {
+        url: "https://res.cloudinary.com/draig/image/upload/v1718564493/mailprex/ecqm348liaxh626yzscs.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -30,10 +98,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={quik.className}>
+        <link rel="canonical" href="https://www.mailprex.top" />
         <AuthProvider>
           <ToastProvider>
             <Navbar />
-
             {children}
             <Footer />
           </ToastProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import Script from "next/script";
 import IndexView from "./views/IndexView/IndexView";
 
 export default function Home() {
-  return <IndexView />;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLEANALYTIC}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+         gtag('config', ${process.env.NEXT_PUBLIC_GOOGLEANALYTIC});
+                  `}
+      </Script>
+      <IndexView />
+    </>
+  );
 }
